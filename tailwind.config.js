88a351a2a1fcd12407088f47981d83b21bde3f20
guibrbs <200037994@aluno.unb.br>
2022-05-19 14:01:20 -0300

    :rocket: Adding new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,30 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          300: '#FBD38D',
+          500: '#F6AD55',
+        },
+        elementary: {
+          300: '#8A5CEF',
+          500: '#744EC9',
+        },
+        light: {
+          300: '#F4EDE4',
+          500: '#F0E7DB',
+        },
+        dark: {
+          300: '#202023',
+          500: '#1D1D20',
+        },
+        textLight: {
+          300: '#f6f6f6',
+          500: '#eeee'
+        }
+      }
+    },
   },
   plugins: [],
 }
