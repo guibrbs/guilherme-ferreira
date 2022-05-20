81f24951f046f75c391dcdd2eb37841e25004cf3
guibrbs <200037994@aluno.unb.br>
2022-05-20 19:39:41 -0300

    :repeat: Changing to next link

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,24 +2,29 @@ import { useTheme } from "next-themes";
 import { Moon, Sun } from "phosphor-react";
 import { useEffect, useState } from "react";
 import { BsGithub } from "react-icons/bs";
+import Link from 'next/link'
 
-export default function Header(){
+export function Header(){
     return(
         <div className="flex justify-center items-center gap-56 w-screen h-16 bg-light-300 dark:bg-dark-500">
             <nav className="flex items-center">
-                <a href="/" className="text-xl font-bold hover:text-dark-300 dark:hover:text-textLight-300 transition-colors mr-9">
-                    Guilherme Ferreira
-                </a>
+                <Link href={"/"}>
+                    <a className="text-xl font-bold hover:text-dark-300 dark:hover:text-textLight-300 transition-colors mr-9">
+                        Guilherme Ferreira
+                    </a>
+                </Link>
                 {[
-                    ['Projetos', '/projects', '1'],
-                    ['Sobre', '/about', '2'],
+                    ['Projetos', '/projetos', '1'],
+                    ['Sobre', '/sobre', '2'],
                     ['Source', 'https://github.com/guibrbs/guilherme-ferreira', '3', ],
                 ].map(([title, url, id]) => (
                     <div className="group relative w-fit mx-4 flex" key={id}>
                         {id === '3' ? <BsGithub style={{marginRight: '4px', marginTop: '4px'}}/> : <></>}
-                        <a href={url} className="font-normal text-base duration-300 hover:text-dark-300 dark:hover:text-textLight-300">
-                            {title}
-                        </a>
+                        <Link href={url}>
+                            <a className="font-normal text-base duration-300 hover:text-dark-300 dark:hover:text-textLight-300">
+                                {title}
+                            </a>
+                        </Link>
                         <span className="absolute -bottom-px left-1/2 w-0 h-px bg-elementary-300 dark:bg-brand-300 group-hover:w-1/2 
                         group-hover:transition-all hover:duration-500 duration-300 ease-in-out"/>
                         <span className="absolute -bottom-px right-1/2 w-0 h-px bg-elementary-300 dark:bg-brand-300 group-hover:w-1/2 
