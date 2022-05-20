2feff6b9be41c699a2384cca42823cf8937f9caa
guibrbs <200037994@aluno.unb.br>
2022-05-20 17:05:55 -0300

    :rocket: Styling nav bar

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,17 +5,19 @@ import { BsGithub } from "react-icons/bs";
 
 export default function Header(){
     return(
-        <div className="flex justify-center items-center gap-36 w-screen h-20 bg-light-300 dark:bg-dark-500">
-            <a href="/" className="text-2xl font-bold hover:text-dark-300 dark:hover:text-textLight-300 transition-colors">Guilherme Ferreira</a>
-            <nav className="flex">
+        <div className="flex justify-center items-center gap-56 w-screen h-16 bg-light-300 dark:bg-dark-500">
+            <nav className="flex items-center">
+                <a href="/" className="text-xl font-bold hover:text-dark-300 dark:hover:text-textLight-300 transition-colors mr-9">
+                    Guilherme Ferreira
+                </a>
                 {[
                     ['Projetos', '/projects', '1'],
                     ['Sobre', '/about', '2'],
-                    ['Source', '/', '3', ],
+                    ['Source', 'https://github.com/guibrbs/guilherme-ferreira', '3', ],
                 ].map(([title, url, id]) => (
                     <div className="group relative w-fit mx-4 flex" key={id}>
-                        {id === '3' ? <BsGithub style={{marginRight: '4px', marginTop: '2.5px'}} /> : <></>}
-                        <a href={url} className="font-normal duration-300 hover:text-dark-300 dark:hover:text-textLight-300">
+                        {id === '3' ? <BsGithub style={{marginRight: '4px', marginTop: '4px'}}/> : <></>}
+                        <a href={url} className="font-normal text-base duration-300 hover:text-dark-300 dark:hover:text-textLight-300">
                             {title}
                         </a>
                         <span className="absolute -bottom-px left-1/2 w-0 h-px bg-elementary-300 dark:bg-brand-300 group-hover:w-1/2 
@@ -39,13 +41,14 @@ const ThemeChanger = () => {
     }
     if (!mounted) return null
     return (
-    <button 
-        onClick={handleTheme} 
-        className="flex items-center justify-center rounded-lg w-10 h-10 text-textLight-500 hover:text-textLight-300
-        bg-elementary-300 hover:bg-elementary-500 transition-colors 
-        dark:bg-brand-300 dark:hover:bg-brand-500 dark:text-dark-500" 
-    >
-        {theme === 'light' ? <Moon weight="fill" /> : <Sun weight="fill" />}
-    </button>
+        <button
+            type="button"
+            onClick={handleTheme} 
+            className="flex items-center justify-center rounded-lg w-8 h-8 text-textLight-500 hover:text-textLight-300
+            bg-elementary-300 hover:bg-elementary-500 transition-colors 
+            dark:bg-brand-300 dark:hover:bg-brand-500 dark:text-dark-500" 
+            >
+            {theme === 'light' ? <Moon weight="fill" /> : <Sun weight="fill" />}
+        </button>
     )
 }
