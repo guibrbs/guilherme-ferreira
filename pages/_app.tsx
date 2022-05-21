c998a53a8b973ff818c7556cbb30833af0ab1ede
guibrbs <200037994@aluno.unb.br>
2022-05-21 19:50:31 -0300

    :rocket: Adding header to all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
+import { Header } from '../components/Header'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return( 
-    <ThemeProvider attribute='class'>
-      <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class">
+        <Header />
+        <Component {...pageProps} />
     </ThemeProvider>
   )
 }
