30e004d41571c3886a46beb1a46a512f5e95829c
guibrbs <200037994@aluno.unb.br>
2022-05-22 18:55:53 -0300

    :rocket: Creating footer component

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <div className="mt-10 flex h-16 w-full items-center justify-center bg-light-100 dark:bg-dark-500">
+      <p className="text-sm font-extralight tracking-wider text-dark-100 opacity-80 dark:text-textLight-600 dark:opacity-50">
+        <span className="font-sans font-thin opacity-70">&copy;</span> 2022 Guilherme Ferreira. All Rights Reserved
+      </p>
+    </div>
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import { Header } from '../components/Header'
+import { Footer } from '../components/Footer'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
         <Header />
         <Component {...pageProps} />
+        <Footer />
     </ThemeProvider>
   )
 }
