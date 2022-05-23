4376d0a54767064f626fbad8c14c636dacfcbafd
guibrbs <200037994@aluno.unb.br>
2022-05-23 15:01:03 -0300

    :rocket: Adding a top line on footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <div className="mt-10 flex h-16 w-full items-center justify-center bg-light-100 dark:bg-dark-500">
+    <div className="mt-10 flex h-16 w-full items-center justify-center bg-light-100 dark:bg-dark-500 border-t border-t-light-100 dark:border-t-dark-100">
       <p className="text-sm font-extralight tracking-wider text-dark-100 opacity-80 dark:text-textLight-600 dark:opacity-50">
         <span className="font-sans font-thin opacity-70">&copy;</span> 2022 Guilherme Ferreira. All Rights Reserved
       </p>
