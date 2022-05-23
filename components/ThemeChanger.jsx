6850edf513ab0014fc30d5fe456768276bcd98d5
guibrbs <200037994@aluno.unb.br>
2022-05-23 20:34:31 -0300

    :rocket: Animating theme button

--- components/ThemeChanger.jsx
+++ components/ThemeChanger.jsx
@@ -1,24 +1,47 @@
-const { useTheme } = require("next-themes")
-const { useEffect, useState } = require("react")
-import { Moon, Sun } from "phosphor-react";
+const { useTheme } = require('next-themes')
+const { useEffect, useState } = require('react')
+import { animate, AnimatePresence, motion } from 'framer-motion'
+import { Moon, Sun } from 'phosphor-react'
 
 export const ThemeChanger = () => {
-    const { theme, setTheme } = useTheme()
-    const [mounted, setMounted] = useState(false)
-    useEffect(() => setMounted(true), [])
-    const handleTheme = () =>{
-        setTheme(theme === 'light' ? 'dark' : 'light')
-    }
-    if (!mounted) return null
-    return (
-        <button
+  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+  const styles = 'flex items-center justify-center rounded-lg w-8 h-8'
+  useEffect(() => setMounted(true), [])
+  const handleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
+  if (!mounted) return null
+  return (
+    <>
+      <AnimatePresence exitBeforeEnter>
+        {theme === 'light' ? (
+          <motion.button
+            key={theme ? 'light' : "empty"}
+            animate={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: 10 }}
+            exit={{ opacity: 0, y: -10 }}
+            transition={{ duration: 0.2 }}
             type="button"
-            onClick={handleTheme} 
-            className="flex items-center justify-center rounded-lg w-8 h-8 text-textLight-500 hover:text-textLight-300
-            bg-elementary-300 hover:bg-elementary-500 transition-colors 
-            dark:bg-brand-300 dark:hover:bg-brand-500 dark:text-dark-500" 
-            >
-            {theme === 'light' ? <Moon weight="fill" /> : <Sun weight="fill" />}
-        </button>
-    )
+            onClick={handleTheme}
+            className={`${styles} bg-elementary-300 text-textLight-500 transition-colors hover:bg-elementary-500 hover:text-textLight-300`}
+          >
+            <Moon weight="fill" />
+          </motion.button>
+        ) : (
+          <motion.button
+            animate={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: 10 }}
+            exit={{ opacity: 0, y: -10 }}
+            transition={{ duration: 0.2 }}
+            type="button"
+            onClick={handleTheme}
+            className={`${styles} bg-brand-300 text-dark-500 transition-colors hover:bg-brand-500`}
+          >
+            <Sun weight="fill" />
+          </motion.button>
+        )}
+      </AnimatePresence>
+    </>
+  )
 }
