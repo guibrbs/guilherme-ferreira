9e228bd74e84533ccfa2f7e41e87f6b496cec7b7
guibrbs <200037994@aluno.unb.br>
2022-05-24 22:11:35 -0300

    :repeat: Adding responsiviness on footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,11 @@
 export function Footer() {
   return (
-    <div className="mt-10 flex h-16 w-full items-center justify-center bg-light-100 dark:bg-dark-500 border-t border-t-light-100 dark:border-t-dark-100">
+    <footer className="mt-10 p-8 flex h-16 w-full items-center justify-center border-t border-t-light-100 
+    bg-light-100 dark:border-t-dark-100 dark:bg-dark-500">
       <p className="text-sm font-extralight tracking-wider text-dark-100 opacity-80 dark:text-textLight-600 dark:opacity-50">
-        <span className="font-sans font-thin opacity-70">&copy;</span> 2022 Guilherme Ferreira. All Rights Reserved
+        <span className="font-sans font-thin opacity-70">&copy;</span>{' '}
+        {new Date().getFullYear()} Guilherme Ferreira. All Rights Reserved
       </p>
-    </div>
+    </footer>
   )
 }
