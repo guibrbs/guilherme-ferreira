4ff3c05da36550074ca5f053b93e428dac97ad1d
guibrbs <200037994@aluno.unb.br>
2022-05-25 20:46:39 -0300

    :rocket: Setting up tech icons

--- components/Projects/ProjectsMap.tsx
+++ components/Projects/ProjectsMap.tsx
@@ -1,7 +1,10 @@
-import { DiHeroku, DiPostgresql } from 'react-icons/di'
+import { DiHeroku, DiPostgresql, DiDocker, DiDjango, DiPython, DiGithubBadge, DiReact, DiNodejs, DiCss3 } from 'react-icons/di'
+import { SiSelenium, SiTypescript, SiJavascript, SiTailwindcss, SiExpress, SiJest, SiEslint, SiMaterialui, SiHtml5 } from 'react-icons/si'
+import { FiFigma } from 'react-icons/fi'
+import { PhosphorLogo } from 'phosphor-react'
 
 type Projects_map = object[]
-const iconStyle = 'w-14 h-14 border-[1px] border-light-700 rounded-md text-4xl flex items-center justify-center'
+const iconStyle = 'w-14 h-14 border-[1px] border-light-700 rounded-md text-4xl flex items-center justify-center mr-4 mt-5 p-1 relative'
 export const ProjectsMap: Projects_map = [
   {
     title: "INDICAA",
@@ -11,17 +14,21 @@ export const ProjectsMap: Projects_map = [
     meio da interface do Metabase.",
     tech: 
       <>
-        <DiHeroku className={iconStyle}/>
-        <DiPostgresql className={iconStyle}/>
-        <i className="devicon-docker-plain colored" title="Docker"></i>
-        <i className="devicon-django-plain colored" title="Django" style={{color: 'green'}}></i>
-        <i className="devicon-selenium-original colored" title="Selenium"></i>
-        <i className="devicon-python-plain colored" title="Python" style={{color: '#FFD300'}}></i>
-        <a href="https://github.com/fga-eps-mds/2021.2-INDICAA" target={"_blank"} rel={"noreferrer noopener"}><i className="devicon-github-original colored"></i></a>
+        <DiHeroku className={`${iconStyle} hover:text-[#400099] transition-colors`} title={'Heroku'} /> 
+        <DiPostgresql className={`${iconStyle} hover:text-[#30628a] transition-colors`} title={'PostgreSQL'} />
+        <DiDocker className={`${iconStyle} hover:text-[#228ee1] transition-colors`} title={'Docker'} />
+        <DiDjango className={`${iconStyle} hover:text-[#092b1d] transition-colors`} title={'Django'} />
+        <SiSelenium className={`${iconStyle} hover:text-[#c4092a] transition-colors`} title={'Selenium'} />
+        <DiPython className={`${iconStyle} hover:text-[#f2ca3b] transition-colors`} title={'Python'} />
+        <FiFigma className={`${iconStyle} hover:text-[#f26c5d] transition-colors`} title={'Figma'} />
+        <a href="https://github.com/fga-eps-mds/2021.2-INDICAA" target={"_blank"} rel={"noreferrer noopener"}>
+          <DiGithubBadge className={`${iconStyle} hover:text-elementary-300 dark:hover:text-brand-300 transition-colors`}/>
+        </a>
       </>,
     img: '/metabase.png',
     link: 'https://fga-eps-mds.github.io/2021.2-INDICAA-Wiki/',
-    id: '1'
+    id: '1',
+    routes: 'indicaa'
   },
   {
     title: "Nlw Impulse",
@@ -31,24 +38,25 @@ export const ProjectsMap: Projects_map = [
     do autor.",
     tech: 
       <>
-        <i className="devicon-react-original colored" title="React"></i>
-        <i className="devicon-javascript-plain colored" title="Javascript" style={{color: "#FFA701"}}></i>
-        <i className="devicon-typescript-plain colored" title="Typescript"></i>
-        <i className="devicon-tailwindcss-plain colored" title="TailwindCSS"></i>
-        <i className="ph-phosphor-logo colored" title="Phosphor" style={{color: 'var(--primary-color)'}}></i>
-        <i className="devicon-express-original colored" title="Express"></i>
-        <i className="devicon-jest-plain colored" title="Jest"></i>
-        <i className="devicon-nodejs-plain-wordmark colored" title="NodeJs"></i>
+        <DiReact className={`${iconStyle} hover:text-[#5ccfee] transition-colors`} title={'React'} />
+        <SiJavascript className={`${iconStyle} hover:text-[#FFA701] transition-colors`} title={'Javascript'} />
+        <SiTypescript className={`${iconStyle} hover:text-[#2f72bc] transition-colors`} title={'Typescript'} />
+        <SiTailwindcss className={`${iconStyle} hover:text-[#35b3eb] transition-colors`} title={'TailwindCSS'} />
+        <PhosphorLogo className={`${iconStyle} hover:text-[#ce93fe] transition-colors`} xlinkTitle={'Phosphor'} weight="fill" />
+        <SiExpress className={`${iconStyle} hover:text-green-600 transition-colors`} title={'Express'} />
+        <SiJest className={`${iconStyle} hover:text-[#c03b13] transition-colors`} title={'Jest'} />
+        <DiNodejs className={`${iconStyle} hover:text-[#7cb900] transition-colors`} title={'Node'} />
         <a href="https://github.com/guibrbs/nlw-impulse-web" target={"_blank"} rel={"noreferrer noopener"}>
-          <i className="devicon-github-original colored"></i>
+          <DiGithubBadge className={`${iconStyle} hover:text-elementary-300 dark:hover:text-brand-300 transition-colors`}/>
         </a>
         <a href="https://github.com/guibrbs/nlw-impulse-server" target={"_blank"} rel={"noreferrer noopener"}>
-         <i className="devicon-postgresql-plain colored"></i>
+          <DiPostgresql className={`${iconStyle} hover:text-[#30628a] transition-colors`} title={'PostgreSQL'} />
         </a>
       </>,
     img: '/nlw-impulse.png',
     link: 'http://nlw-impulse-web-six.vercel.app/',
-    id: '2'
+    id: '2',
+    routes: 'nlw'
   },
   {
     title: "Sign In | Sign Up",
@@ -57,14 +65,17 @@ export const ProjectsMap: Projects_map = [
     referentes às tecnologias utilizadas, procurando seguir boas práticas de programação e construir um código limpo.",
     tech: 
       <>
-        <i className="devicon-react-original colored" title="React"></i>
-        <i className="devicon-javascript-plain colored" title="Javascript" style={{color: "#FFA701"}}></i>
-        <i className="devicon-css3-plain colored" title="CSS"></i>
-        <a href="https://github.com/guibrbs/Sliding-Sign-In" target={"_blank"} rel={"noreferrer noopener"}><i className="devicon-github-original colored"></i></a>
+        <DiReact className={`${iconStyle} hover:text-[#5ccfee] transition-colors`} title={'React'} />
+        <SiJavascript className={`${iconStyle} hover:text-[#FFA701] transition-colors`} title={'Javascript'} />
+        <DiCss3 className={`${iconStyle} hover:text-blue-600 transition-colors`} title={'CSS3'} />
+        <a href="https://github.com/guibrbs/Sliding-Sign-In" target={"_blank"} rel={"noreferrer noopener"}>
+          <DiGithubBadge className={`${iconStyle} hover:text-elementary-300 dark:hover:text-brand-300 transition-colors`}/>
+        </a>
       </>,
     img: '/sliding-signin.png',
     link: 'https://guibrbs.github.io/Sliding-Sign-In/',
-    id: '3'
+    id: '3',
+    routes: 'signin'
   },
   {
     title: "Landing Page",
@@ -73,15 +84,18 @@ export const ProjectsMap: Projects_map = [
     além de aprender a realizar a troca para o modo noturno, no qual o mesmo mecanismo de troca foi utilizado neste portfólio.",
     tech: 
       <>
-        <i className="devicon-react-original colored" title="React"></i>
-        <i className="devicon-javascript-plain colored" title="Javascript" style={{color: "#FFA701"}}></i>
-        <i className="devicon-typescript-plain colored" title="Typescript"></i>
-        <i className="devicon-css3-plain colored" title="CSS"></i>
-        <a href="https://github.com/guibrbs/landing_page" target={"_blank"} rel={"noreferrer noopener"}><i className="devicon-github-original colored"></i></a>
+        <DiReact className={`${iconStyle} hover:text-[#5ccfee] transition-colors`} title={'React'} />
+        <SiJavascript className={`${iconStyle} hover:text-[#FFA701] transition-colors`} title={'Javascript'} />
+        <SiTypescript className={`${iconStyle} hover:text-[#2f72bc] transition-colors`} title={'Typescript'} />
+        <DiCss3 className={`${iconStyle} hover:text-blue-600 transition-colors`} title={'CSS3'} />
+        <a href="https://github.com/guibrbs/landing_page" target={"_blank"} rel={"noreferrer noopener"}>
+         <DiGithubBadge className={`${iconStyle} hover:text-elementary-300 dark:hover:text-brand-300 transition-colors`}/>
+        </a>
       </>,
-    img: '/landing-page',
+    img: '/landing-page.png',
     link: 'https://guibrbs.github.io/landing_page/',
-    id: '4'
+    id: '4',
+    routes: 'landing-page'
   },
   {
     title: "Clone Netflix",
@@ -90,16 +104,19 @@ export const ProjectsMap: Projects_map = [
     foram coletadas através da API TMDB, contudo vale ressaltar que não são inclusos os catálogos disponibilizados oficialmente pela Netflix.",
     tech: 
       <>
-        <i className="devicon-react-original colored" title="React"></i>
-        <i className="devicon-javascript-plain colored" title="Javascript" style={{color: "#FFA701"}}></i>
-        <i className="devicon-eslint-original colored" title="Eslint"></i>
-        <i className="devicon-materialui-plain colored" title="MaterialUI"></i>
-        <i className="devicon-css3-plain colored" title="CSS"></i>
-        <a href="https://github.com/guibrbs/ProjetoNetflix" target={"_blank"} rel={"noreferrer noopener"}><i className="devicon-github-original colored"></i></a>
+        <DiReact className={`${iconStyle} hover:text-[#5ccfee] transition-colors`} title={'React'} />
+        <SiJavascript className={`${iconStyle} hover:text-[#FFA701] transition-colors`} title={'Javascript'} />
+        <SiEslint className={`${iconStyle} hover:text-[#482fbd] transition-colors`} title={'Eslint'} />
+        <SiMaterialui className={`${iconStyle} hover:text-[#007dc5] transition-colors`} title={'MaterialUI'} />
+        <DiCss3 className={`${iconStyle} hover:text-blue-600 transition-colors`} title={'CSS3'} />
+        <a href="https://github.com/guibrbs/ProjetoNetflix" target={"_blank"} rel={"noreferrer noopener"}>
+          <DiGithubBadge className={`${iconStyle} hover:text-elementary-300 dark:hover:text-brand-300 transition-colors`}/>
+        </a>
       </>,
     img: '/clone-netflix.png',
     link: 'https://guibrbs.github.io/ProjetoNetflix/',
-    id: '5'
+    id: '5',
+    routes: 'clone-netflix'
   },
   {
     title: "Interactive Rating",
@@ -108,14 +125,17 @@ export const ProjectsMap: Projects_map = [
     e com alguns conhecimentos prévios das técnologias utilizadas.",
     tech: 
       <>
-        <i className="devicon-javascript-plain colored" title="Javascript" style={{color: "#FFA701"}}></i>
-        <i className="devicon-css3-plain colored" title="CSS"></i>
-        <i className="devicon-html5-plain colored" title="HTML"></i>
-        <a href="https://github.com/guibrbs/Interacting_rating_component" target={"_blank"} rel={"noreferrer noopener"}><i className="devicon-github-original colored"></i></a>
+        <SiJavascript className={`${iconStyle} hover:text-[#FFA701] transition-colors`} title={'Javascript'} />
+        <DiCss3 className={`${iconStyle} hover:text-blue-600 transition-colors`} title={'CSS3'} />
+        <SiHtml5 className={`${iconStyle} hover:text-[#e96228] transition-colors`} title={'HTML5'} />
+        <a href="https://github.com/guibrbs/Interacting_rating_component" target={"_blank"} rel={"noreferrer noopener"}>
+          <DiGithubBadge className={`${iconStyle} hover:text-elementary-300 dark:hover:text-brand-300 transition-colors`}/>
+        </a>
       </>,
     img: '/interactive-rating.png',
     link: 'https://guibrbs.github.io/Interacting_rating_component/',
-    id: '6'
+    id: '6',
+    routes: 'interactive-rating'
   },
   {
     title: "Covid-19 Tracker",
@@ -124,14 +144,17 @@ export const ProjectsMap: Projects_map = [
     O objetivo com o desenvolvimento desse projeto foi aprender a coletar dados de API e representar graficamente.",
     tech: 
       <>
-        <i className="devicon-react-original colored" title="React"></i>
-        <i className="devicon-javascript-plain colored" title="Javascript" style={{color: "#FFA701"}}></i>
-        <i className="devicon-materialui-plain colored" title="MaterialUI"></i>
-        <i className="devicon-css3-plain colored" title="CSS"></i>
-        <a href="https://github.com/guibrbs/Covid-Tracker" target={"_blank"} rel={"noreferrer noopener"}><i className="devicon-github-original colored"></i></a>
+        <DiReact className={`${iconStyle} hover:text-[#5ccfee] transition-colors`} title={'React'} />
+        <SiJavascript className={`${iconStyle} hover:text-[#FFA701] transition-colors`} title={'Javascript'} />
+        <SiMaterialui className={`${iconStyle} hover:text-[#007dc5] transition-colors`} title={'MaterialUI'} />
+        <DiCss3 className={`${iconStyle} hover:text-blue-600 transition-colors`} title={'CSS3'} />
+        <a href="https://github.com/guibrbs/Covid-Tracker" target={"_blank"} rel={"noreferrer noopener"}>
+          <DiGithubBadge className={`${iconStyle} hover:text-elementary-300 dark:hover:text-brand-300 transition-colors`}/>
+        </a>
       </>,
     img: '/covid-tracker.png',
     link: 'https://guibrbs.github.io/Covid-Tracker/',
-    id: '7'
+    id: '7',
+    routes: 'covid-tracker'
   },
 ]
--- components/Projects/index.jsx
+++ components/Projects/index.jsx
@@ -5,37 +5,52 @@ import { BsBoxArrowInUpRight } from 'react-icons/bs'
 import Link from 'next/link'
 
 export function Projects() {
-    const routes = useRouter()
-    return (
-        <div className='w-full h-fit flex justify-center mt-36'>
-            {ProjectsMap.filter(banner => banner.id == 1).map((project) => {
-                return(
-                    <div className='w-full max-w-customWidth' key={project.id}>
-                        <div className='flex items-center space-x-4'>
-                            <Link href={'/projetos'}>
-                                <a className='text-xl text-elementary-300 underline'>Projetos</a>
-                            </Link>
-                            <IoIosArrowForward />
-                            <Link href={project.link}>
-                                <div className='flex items-center justify-center cursor-pointer'>
-                                    <a className='font-bold text-3xl' target="_blank" rel="noopener noreferrer">{project.title}</a>
-                                    <BsBoxArrowInUpRight className='ml-1 text-2xl'/>
-                                </div>
-                            </Link>
-                        </div>
-                        <p className='font-normal text-xl leading-7 text-justify mt-5'>{project.description}</p>
-                        <div className='w-full flex justify-between mt-3'>
-                            <div>
-                                <h1 className='font-bold text-[1.75rem]'>Tecnologias utilizadas</h1>
-                                <p className='flex space-x-4'>
-                                    {project.tech}
-                                </p>
-                            </div>
-                            <img src={project.img} alt={project.alt} className='rounded-xl w-full max-w-[400px]'/>
-                        </div>
-                    </div>
-                )
-            })}
-        </div>
-    )
+  const routes = useRouter()
+  const {slug} = routes.query
+  return (
+    <div className="mt-36 flex h-fit w-full justify-center">
+      {ProjectsMap.map((project) => {
+        if(slug === project.routes)
+        return (
+          <div className="w-full max-w-customWidth" key={project.id}>
+            <div className="flex items-center space-x-4">
+              <Link href={'/projetos'}>
+                <a className="text-xl text-elementary-300 dark:text-brand-300 underline">
+                  Projetos
+                </a>
+              </Link>
+              <IoIosArrowForward />
+              <div className="flex cursor-pointer items-center justify-center">
+                <a
+                  href={project.link}
+                  className="text-3xl font-bold flex items-center justify-center"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {project.title}
+                  <BsBoxArrowInUpRight className="ml-1 text-2xl" />
+                </a>
+              </div>
+            </div>
+            <p className="mt-5 text-justify text-xl font-normal leading-7 dark:text-textLight-600">
+              {project.description}
+            </p>
+            <div className="mt-3 flex w-full justify-between">
+              <div className="flex w-full max-w-xs flex-col flex-wrap">
+                <h1 className="text-[1.75rem] font-bold ">
+                  Tecnologias utilizadas
+                </h1>
+                <div className="flex flex-wrap justify-center">{project.tech}</div>
+              </div>
+              <img
+                src={project.img}
+                alt={project.alt}
+                className="w-full max-w-[400px] h-full max-h-[272px] rounded-xl"
+              />
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
