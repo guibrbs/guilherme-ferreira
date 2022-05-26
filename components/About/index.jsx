1b1ed6761e5d3a59ba4d9698e6ba8288b4eea929
guibrbs <200037994@aluno.unb.br>
2022-05-25 21:26:16 -0300

    :rocket: Adding links

--- components/About/index.jsx
+++ components/About/index.jsx
@@ -1,19 +1,31 @@
+import { BsFileTextFill } from 'react-icons/bs'
+import { SiGithub, SiGmail, SiLinkedin, SiMailchimp } from 'react-icons/si'
 import { Habilities } from './Habilities'
 
 export function About() {
   return (
     <div className="mt-32 flex h-full w-full justify-center">
       <section className="h-max w-full max-w-customWidth">
-        <h1 className="flex flex-col items-center text-center text-6xl font-bold">
-          <span className="w-fit rounded-tl-[14px] rounded-tr-[50px] rounded-br-[10px] rounded-bl-[50px] bg-brand-300 dark:bg-elementary-500 px-6 pt-2">
-            Web &amp; Mobile
-          </span>
-          <span className="underline decoration-elementary-300 dark:decoration-brand-300">Developer</span>
-        </h1>
-        <div className="relative mt-9 flex items-center after:absolute after:-bottom-4 after:h-px 
-        after:w-full after:bg-light-700 after:content-[''] dark:after:bg-dark-100">
-          <img className="h-14 max-h-[56px] w-full max-w-[56px] rounded-full bg-[url('/photo.jpg')] bg-cover bg-center bg-no-repeat" />
-          <h2 className="ml-2 text-2xl font-bold">Sobre</h2>
+        <div className="relative mt-9 flex items-end after:absolute after:-bottom-4 after:h-px 
+          after:w-full after:bg-light-700 after:content-[''] dark:after:bg-dark-100 justify-between">
+          <div className='flex w-full items-end'>
+            <img className="h-14 max-h-[56px] w-full max-w-[56px] rounded-full bg-[url('/photo.jpg')] bg-cover bg-center bg-no-repeat" />
+            <h2 className="ml-2 text-2xl font-bold">Guilherme Ferreira</h2>
+          </div>
+          <div className='space-x-4 flex'>
+            <a href="https://github.com/guibrbs" target={'_blank'} className='hover:brightness-200'>
+              <SiGithub size={20}/>
+            </a>
+            <a href="https://www.linkedin.com/in/guibrbs/" target={'_blank'} className='hover:brightness-200'>
+              <SiLinkedin size={20}/>
+            </a>
+            <a href={"mailto: mrguilhermeferreira@gmail.com"} target={'_blank'} className='hover:brightness-200'>
+              <SiGmail size={20}/>
+            </a>
+            <a href="/CurriculoGuilhermeFerreira.pdf" target={'_blank'} className='hover:brightness-200'>
+              <BsFileTextFill size={20}/>
+            </a>
+          </div>
         </div>
         <p className="mt-7 text-justify text-xl leading-[22.5px] tracking-wide text-dark-100 dark:text-textLight-600">
           Eu sou o Guilherme, sou{' '}
