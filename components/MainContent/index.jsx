1a4c29ec1ae4c46189fc171ba926666a61459968
guibrbs <200037994@aluno.unb.br>
2022-05-26 21:13:48 -0300

    :repeat: Style changes

--- components/MainContent/index.jsx
+++ components/MainContent/index.jsx
@@ -10,14 +10,14 @@ export function MainContent(){
                 after:content[""] after:w-full after:h-px after:bg-light-700 dark:after:bg-dark-100 after:-bottom-16'>
                 <Avatar />
                 <div className='w-full max-w-txtWidth'>
-                    <p className='text-5xl font-bold '>Guilherme Ferreira</p>
+                    <p className='text-[2.9rem] leading-[2.9rem] font-bold '>Guilherme Ferreira</p>
                     <p className='text-2xl font-light dark:text-textLight-600'>Web &amp; Mobile Developer (Fullstack)</p>
                     <p className='w-full font-light text-2xl mt-6 dark:text-textLight-600 text-dark-500 leading-7'>
-                        Sou estudante de <b className='dark:text-textLight-500 font-bold'><i>Engenharia de Software</i></b> pela 
-                        <b className='dark:text-textLight-500 font-bold'><i> Universidade de Brasília</i></b>. 
+                        Sou estudante de <b className='dark:text-textLight-500 font-bold'>Engenharia de Software</b> pela 
+                        <b className='dark:text-textLight-500 font-bold'> Universidade de Brasília</b>. 
                         Esse é o meu portfólio, onde você consegue encontrar alguns de meus projetos 
-                        como <b className='dark:text-textLight-500 font-bold'><i>Desenvolvedor Web</i></b>. Se estiver procurando alguém 
-                        <b className='dark:text-textLight-500 font-bold'><i> competente e focado</i></b>, hoje é seu dia de sorte, pois esse sou eu!
+                        como <b className='dark:text-textLight-500 font-bold'>Desenvolvedor Web</b>. Se estiver procurando alguém 
+                        <b className='dark:text-textLight-500 font-bold'> competente e focado</b>, hoje é seu dia de sorte, pois esse sou eu!
                     </p>
                     <div className='flex mt-7 items-center space-x-3'>
                         <button className='w-52 h-12 dark:bg-textLight-500 bg-dark-500 dark:text-dark-500 text-textLight-500 
