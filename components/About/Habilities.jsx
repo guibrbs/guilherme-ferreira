bf313a92f00f6a3a47d5c4affa7a27e1959633fd
guibrbs <200037994@aluno.unb.br>
2022-05-26 21:14:47 -0300

    :rocket: Finishing about page

--- components/About/Habilities.jsx
+++ components/About/Habilities.jsx
@@ -1,58 +1,125 @@
-import { SiTypescript, SiJavascript, SiNextdotjs, SiTailwindcss, SiCss3, SiNodedotjs, SiReact, SiMongodb, SiReactivex, SiExpo, SiStyledcomponents, SiFigma, SiAdobephotoshop, SiAdobexd, SiFramer } from "react-icons/si"
-const iconStyles = "w-10 h-10 border border-light-700 dark:border-dark-100 rounded-md p-1 mt-3 overflow-hidden"
+import {
+  SiTypescript,
+  SiJavascript,
+  SiNextdotjs,
+  SiTailwindcss,
+  SiCss3,
+  SiNodedotjs,
+  SiReact,
+  SiMongodb,
+  SiExpo,
+  SiFigma,
+  SiAdobephotoshop,
+  SiAdobexd,
+  SiFramer,
+} from 'react-icons/si'
+
+const iconStyles = 'w-10 h-10 border border-light-700 dark:border-dark-100 rounded-md p-1 mt-3 overflow-hidden'
+const contentStyle = 'mt-2 text-sm leading-[15px] dark:text-textLight-600'
+const boldStyle = 'font-bold dark:font-medium dark:text-textLight-300'
 const habilities = [
-    {
-        title: 'Web Development',
-        content: 'lobortis, varius adipiscing viverra Lorem laoreet sollicitudin. non Cras sodales. cursus Nullam efficitur. tincidunt Donec',
-        tech: 
-            <>
-                <SiReact className= {iconStyles} color={'#5ccfee'} title={'ReactJS'}/>
-                <SiJavascript className={iconStyles} color={'#FFA701'} title={'Javascript'} />
-                <SiTypescript className={iconStyles} color={'#2f72bc'} title={'Typescript'} />
-                <SiNextdotjs className={iconStyles} color={'black'} title={'Next Js'} />
-                <SiTailwindcss className={iconStyles} color={'#35b3eb'} title={'TailwindCSS'} />
-                <SiCss3 className={iconStyles} color={'#35b3e9'} title={'CSS3'} />
-                <SiNodedotjs className={iconStyles} color={'green'} title={'NodeJS'} />
-                <SiMongodb className={iconStyles} color={'#293'} title={'MongoDB'} />
-            </>
-    },
-    {
-        title: 'Mobile Development',
-        content: 'lobortis, varius adipiscing viverra Lorem laoreet sollicitudin. non Cras sodales. cursus Nullam efficitur. tincidunt Donec',
-        tech: 
-            <>
-                <SiReact className= {iconStyles} color={'#5ccfee'} title={'ReactJS'}/>
-                <SiJavascript className={iconStyles} color={'#FFA701'} title={'Javascript'} />
-                <SiTypescript className={iconStyles} color={'#2f72bc'} title={'Typescript'} />
-                <SiExpo className={iconStyles} title={'Expo'} />
-            </>
-    },
-    {
-        title: 'Prototyping',
-        content: 'lobortis, varius adipiscing viverra Lorem laoreet sollicitudin. non Cras sodales. cursus Nullam efficitur. tincidunt Donec',
-        tech: 
-            <>
-                <SiFigma className={iconStyles} color={'#f26c5d'} title={'Figma'} />
-                <SiAdobephotoshop className={iconStyles} color={'royalblue'} title={'Photoshop'} />
-                <SiAdobexd className={iconStyles} color={'#f75eee'} title={'Adobe XD'} />
-                <SiFramer className={iconStyles} color={'#62d0f2'} title={'Framer'} />
-            </>
-    },
+  {
+    title: 'Web Development',
+    content:
+      <p className={contentStyle}>
+          Meus projetos web são <span className={boldStyle}>totalmente responsíveis</span>, com 
+          <span className={boldStyle}> acessibilidade</span>, 
+          certificando que irá renderizar <span className={boldStyle}>igualmente</span> para todos os browsers.
+      </p>,
+    tech: (
+      <>
+        <SiReact className={iconStyles} color={'#5ccfee'} title={'ReactJS'} />
+        <SiJavascript
+          className={iconStyles}
+          color={'#FFA701'}
+          title={'Javascript'}
+        />
+        <SiTypescript
+          className={iconStyles}
+          color={'#2f72bc'}
+          title={'Typescript'}
+        />
+        <SiNextdotjs className={iconStyles} color={'black'} title={'Next Js'} />
+        <SiTailwindcss
+          className={iconStyles}
+          color={'#35b3eb'}
+          title={'TailwindCSS'}
+        />
+        <SiCss3 className={iconStyles} color={'#35b3e9'} title={'CSS3'} />
+        <SiNodedotjs className={iconStyles} color={'green'} title={'NodeJS'} />
+        <SiMongodb className={iconStyles} color={'#293'} title={'MongoDB'} />
+      </>
+    ),
+  },
+  {
+    title: 'Mobile Development',
+    content: (
+      <p className={contentStyle}>
+        A base para todos os meus projetos mobile é o foco em <span className={boldStyle}>acessibilidade </span> 
+        e <span className={boldStyle}>UI/UX desing.</span>
+      </p>
+    ),
+    tech: (
+      <>
+        <SiReact className={iconStyles} color={'#5ccfee'} title={'ReactJS'} />
+        <SiJavascript
+          className={iconStyles}
+          color={'#FFA701'}
+          title={'Javascript'}
+        />
+        <SiTypescript
+          className={iconStyles}
+          color={'#2f72bc'}
+          title={'Typescript'}
+        />
+        <SiExpo className={iconStyles} title={'Expo'} />
+      </>
+    ),
+  },
+  {
+    title: 'Prototyping',
+    content:
+      <p className={contentStyle}>
+        Nas minhas prototipagens eu tento buscar <span className={boldStyle}>inspirações modernas</span>  e que fazem sentido 
+        ao meu contexto, com o máximo de <span className={boldStyle}> foco na experiência do usuário.</span>
+      </p>,
+    tech: (
+      <>
+        <SiFigma className={iconStyles} color={'#f26c5d'} title={'Figma'} />
+        <SiAdobephotoshop
+          className={iconStyles}
+          color={'royalblue'}
+          title={'Photoshop'}
+        />
+        <SiAdobexd
+          className={iconStyles}
+          color={'#f75eee'}
+          title={'Adobe XD'}
+        />
+        <SiFramer className={iconStyles} color={'#62d0f2'} title={'Framer'} />
+      </>
+    ),
+  },
 ]
 
 export function Habilities() {
-    return(
-        <section className="w-full flex justify-between mt-4 flex-wrap">
-            {habilities.map((habilities) => {
-                return(
-                    <div className="w-full h-full max-w-[220px] min-h-[256px] border border-light-700
-                     dark:border-dark-100 rounded-xl p-4 flex flex-col">
-                        <h3 className="text-xl">{habilities.title}</h3>
-                        <p className="dark:text-textLight-600 leading-[15px] text-sm mt-2">{habilities.content}</p>
-                        <div className="items-end justify-between flex flex-wrap">{habilities.tech}</div>
-                    </div>
-                )
-            })}
-        </section>
-    )
+  return (
+    <section className="mt-4 flex w-full flex-wrap justify-between">
+      {habilities.map((habilities, key) => {
+        return (
+          <div
+            className="flex h-[256px] w-full max-w-[220px] flex-col justify-center
+                     rounded-xl border border-light-700 p-4 dark:border-dark-100"
+            key={key}
+          >
+            <h3 className="text-lg font-semibold">{habilities.title}</h3>
+            {habilities.content}
+            <div className="flex h-full flex-wrap items-end justify-between">
+              {habilities.tech}
+            </div>
+          </div>
+        )
+      })}
+    </section>
+  )
 }
--- components/About/index.jsx
+++ components/About/index.jsx
@@ -1,53 +1,91 @@
 import { BsFileTextFill } from 'react-icons/bs'
-import { SiGithub, SiGmail, SiLinkedin, SiMailchimp } from 'react-icons/si'
+import { SiGithub, SiGmail, SiLinkedin } from 'react-icons/si'
 import { Habilities } from './Habilities'
 
 export function About() {
+  const textBold = 'font-bold dark:text-textLight-500'
+  const paragraphStyle =
+    'mt-7 text-justify text-xl leading-[22.5px] tracking-wide text-dark-100 dark:text-textLight-600'
   return (
-    <div className="mt-32 flex h-full w-full justify-center">
+    <div className="mt-14 flex h-full w-full justify-center">
       <section className="h-max w-full max-w-customWidth">
-        <div className="relative mt-9 flex items-end after:absolute after:-bottom-4 after:h-px 
-          after:w-full after:bg-light-700 after:content-[''] dark:after:bg-dark-100 justify-between">
-          <div className='flex w-full items-end'>
+        <div
+          className="relative mt-9 flex items-end justify-between after:absolute after:-bottom-4 
+          after:h-px after:w-full after:bg-light-700 after:content-[''] dark:after:bg-dark-100"
+        >
+          <div className="flex w-full items-end">
             <img className="h-14 max-h-[56px] w-full max-w-[56px] rounded-full bg-[url('/photo.jpg')] bg-cover bg-center bg-no-repeat" />
             <h2 className="ml-2 text-2xl font-bold">Guilherme Ferreira</h2>
           </div>
-          <div className='space-x-4 flex'>
-            <a href="https://github.com/guibrbs" target={'_blank'} className='hover:brightness-200'>
-              <SiGithub size={20}/>
+          <div className="flex space-x-4">
+            <a
+              href="https://github.com/guibrbs"
+              target={'_blank'}
+              className="hover:brightness-200"
+            >
+              <SiGithub size={20} />
             </a>
-            <a href="https://www.linkedin.com/in/guibrbs/" target={'_blank'} className='hover:brightness-200'>
-              <SiLinkedin size={20}/>
+            <a
+              href="https://www.linkedin.com/in/guibrbs/"
+              target={'_blank'}
+              className="hover:brightness-200"
+            >
+              <SiLinkedin size={20} />
             </a>
-            <a href={"mailto: mrguilhermeferreira@gmail.com"} target={'_blank'} className='hover:brightness-200'>
-              <SiGmail size={20}/>
+            <a
+              href={'mailto: mrguilhermeferreira@gmail.com'}
+              target={'_blank'}
+              className="hover:brightness-200"
+            >
+              <SiGmail size={20} />
             </a>
-            <a href="/CurriculoGuilhermeFerreira.pdf" target={'_blank'} className='hover:brightness-200'>
-              <BsFileTextFill size={20}/>
+            <a
+              href="/CurriculoGuilhermeFerreira.pdf"
+              target={'_blank'}
+              className="hover:brightness-200"
+            >
+              <BsFileTextFill size={20} />
             </a>
           </div>
         </div>
-        <p className="mt-7 text-justify text-xl leading-[22.5px] tracking-wide text-dark-100 dark:text-textLight-600">
-          Eu sou o Guilherme, sou{' '}
-          <span className="font-extrabold dark:text-textLight-500">técnico em informática</span> e
-          atualmente estou fazendo graduação em{' '}
-          <span className="font-extrabold dark:text-textLight-500">Engenharia de Software</span> pela
-          <span className="font-extrabold dark:text-textLight-500"> Universidade de Brasília. </span>
-          Pretendo seguir a área de desenvolvimento{' '}
-          <span className="font-extrabold dark:text-textLight-500">Web e Mobile</span> com a ambição de
-          participar das equipes de desenvolvimento de{' '}
-          <span className="font-extrabold dark:text-textLight-500">grandes empresas</span>, me tornando
-          uma <span className="font-extrabold dark:text-textLight-500">referência</span> para os
-          profissionais da área. Atualmente sou trainee na{' '}
-          <a
-            href="https://orcestra.com.br/"
-            className="font-extrabold underline decoration-1 dark:text-textLight-500"
-            target={'_blank'}
-          >
-            Orc'estra Gamificação.
-          </a>
-        </p>
-        <h2 className="text-2xl font-bold mt-7">Habilidades</h2>
+        <div className={`${paragraphStyle} `}>
+          <p>
+            Eu sou o Guilherme, sou{' '}
+            <span className={textBold}>técnico em informática</span> e atualmente
+            estou fazendo graduação em{' '}
+            <span className={textBold}>Engenharia de Software</span> pela
+            <span className={textBold}> Universidade de Brasília. </span>
+          </p>
+          <p className='mt-4'>
+            Em <span className={textBold}>2017</span> tive meu primeiro contato com <span className={textBold}> programação</span> dentro do Ensino
+            Médio, no qual eu cursava juntamente o curso de técnico em
+            informática, e através dos <span className={textBold}> conteúdos passados</span> pelos professores, eu me
+            apaixonei pelo desenvolvimento web, e através dessa experiência, em
+            <span className={textBold}> 2020</span> eu passei na Universidade de Brasília para cursar Engenharia de
+            Software, e até então estou <span className={textBold}> amando o curso.</span>
+          </p>
+          <p className='mt-4'>
+            Pretendo seguir a área de desenvolvimento{' '}
+            <span className={textBold}>Web e Mobile</span> com a ambição de
+            participar das equipes de desenvolvimento de{' '}
+            <span className={textBold}>grandes empresas</span>, me tornando uma{' '}
+            <span className={textBold}>referência</span> para os profissionais da
+            área.
+          </p>
+          <p className='mt-4'>
+            Atualmente sou trainee na{' '}
+            <a
+              href="https://orcestra.com.br/"
+              className="font-extrabold underline decoration-1 dark:text-textLight-500"
+              target={'_blank'}
+            >
+              Orc'estra Gamificação 
+            </a>
+            {' '} a <span className={textBold}>primeira empresa júnior de gamificação do Brasil</span> e está sendo um
+            momento muito <span className={textBold}> prazeroso e divertido.</span>
+          </p>
+        </div>
+        <h2 className="mt-7 text-2xl font-bold">Habilidades</h2>
         <Habilities />
       </section>
     </div>
