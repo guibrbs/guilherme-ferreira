f3a30e1b7e04f23d667a9f5c10614071f8d5884c
guibrbs <200037994@aluno.unb.br>
2022-05-27 20:41:29 -0300

    :rocket: Adding mobile responsiviness on header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,36 @@
 import { ThemeChanger } from './ThemeChanger'
 import { Navigation } from './Navigation'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
-export function Header(){
-    return(
-        <header className="flex justify-center items-center gap-56 w-screen h-16 bg-light-300/90 dark:bg-dark-500/90 fixed top-0 z-20 backdrop-blur-md">
-            <nav className="flex items-center">
-                <Link href={"/"}>
-                    <a className="text-xl font-bold hover:text-dark-300 dark:hover:text-textLight-300 transition-colors mr-9">
-                        Guilherme Ferreira
-                    </a>
-                </Link>
-                <Navigation />
-            </nav>
-            <ThemeChanger />
-        </header>
-    )
+export function Header() {
+  const [hamburguerMenu, setHamburguerMenu] = useState(false)
+  const hamburguerMenuActive = () => {
+    setHamburguerMenu(!hamburguerMenu)
+  }
+  return (
+    <header
+      className="fixed top-0 z-20 flex h-16 w-screen items-center 
+        justify-center gap-56 bg-light-300/90 px-10 backdrop-blur-md dark:bg-dark-500/90 max-md:justify-between max-md:gap-0"
+    >
+      <nav className="flex items-center">
+        <Link href={'/'}>
+          <a className="mr-9 text-xl font-bold transition-colors hover:text-dark-300 dark:hover:text-textLight-300 max-md:mr-0">
+            Guilherme Ferreira
+          </a>
+        </Link>
+        <Navigation hamburguerMenu={hamburguerMenu}/>
+      </nav>
+      <ThemeChanger />
+      <div className='hidden w-[150px] max-md:flex justify-center'>
+        <span className={`hidden max-md:block w-9 h-1 relative bg-dark-300 dark:bg-textLight-500 rounded-md  
+        after:content-[""] after:absolute after:w-full after:h-1 after:bg-dark-300 after:dark:bg-textLight-500 after:mt-2 after:rounded-md
+        before:content-[""] before:absolute before:w-full before:h-1 before:bg-dark-300 before:dark:bg-textLight-500 before:-mt-2 before:rounded-md
+        ${hamburguerMenu ? 'after:transform after:rotate-45 before:-rotate-45 after:mt-0 before:mt-0 bg-opacity-0 dark:bg-opacity-0 transition' : ''}
+        `}
+        onClick={hamburguerMenuActive} 
+        />
+      </div>
+    </header>
+  )
 }
--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,10 +1,10 @@
 import { LayoutGroup } from 'framer-motion'
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { BsGithub } from "react-icons/bs";
-import { UnderlineLayout } from './UnderlineLayout';
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { BsGithub } from 'react-icons/bs'
+import { UnderlineLayout } from './UnderlineLayout'
 
-export function Navigation() {
+export function Navigation( { hamburguerMenu } ) {
   const router = useRouter()
   const links = [
     {
@@ -25,25 +25,31 @@ export function Navigation() {
   ]
   return (
     <LayoutGroup>
-      {links.map(({ name, href, id }) => (
-        <div className="group relative mx-4 flex w-fit" key={id}>
-          {id === 3 ? (
-            <BsGithub style={{ marginRight: '4px', marginTop: '4px' }} />
-          ) : (
-            <></>
-          )}
-          <Link href={href}>
-            <a
-              className="text-base font-normal duration-300 hover:text-dark-300 dark:hover:text-textLight-300"
-              target={id === 3 ? '_blank' : ''}
-              rel={id === 3 ? 'noopener noreferrer' : ''}
-            >
-              {name}
-            </a>
-          </Link>
-          <UnderlineLayout href={href}/>
-        </div>
-      ))}
+      <div className={`flex max-md:fixed max-md:-bottom-14 max-md:-right-28 max-md:opacity-0 max-md:bg-light-100 max-md:dark:bg-dark-100 max-md:p-2
+       max-md:rounded-md duration-300 pointer-events-none 
+      ${hamburguerMenu ? 'max-md:right-2 max-md:opacity-100 pointer-events-auto' : ''}`}>
+        {links.map(({ name, href, id }) => {
+          return (
+            <div key={id} className='group relative mx-4 flex w-fit'>
+              {id === 3 ? (
+                <BsGithub style={{ marginRight: '4px', marginTop: '4px' }}/>
+              ) : (
+                <></>
+              )}
+              <Link href={href} key={id}>
+                <a
+                  className="text-base font-normal duration-300 hover:text-dark-300 dark:hover:text-textLight-300"
+                  target={id === 3 ? '_blank' : ''}
+                  rel={id === 3 ? 'noopener noreferrer' : ''}
+                >
+                  {name}
+                </a>
+              </Link>
+              <UnderlineLayout href={href} />
+            </div>
+          )
+        })}
+      </div>
     </LayoutGroup>
   )
 }
