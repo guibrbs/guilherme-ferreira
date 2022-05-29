0ad47159341cb4461e36d69996df0286f271bd99
guibrbs <200037994@aluno.unb.br>
2022-05-29 20:56:01 -0300

    :rocket: Adding more mobile responsiviness on curiosity section

--- components/CuriositySection/index.jsx
+++ components/CuriositySection/index.jsx
@@ -7,11 +7,12 @@ export function CuriositySection() {
     <div className="mt-32 flex h-full w-full justify-center">
       <section className="flex h-full max-h-max w-full max-w-customWidth items-center justify-evenly relative 
       after:content[''] after:w-full after:h-px after:bg-light-700 after:dark:bg-dark-100 after:absolute after:-bottom-16 max-md:flex-col">
-        <div className="w-72">
-          <p className="flex h-14 w-full max-w-[238px] items-center justify-center rounded-lg bg-light-100 text-3xl font-bold dark:bg-dark-100">
+        <div className="w-72 max-md:w-full max-md:max-w-[293px]">
+          <p className="flex h-14 w-full max-w-[238px] items-center justify-center rounded-lg bg-light-100 text-3xl font-bold dark:bg-dark-100
+          max-md:m-auto">
             CURIOSIDADE
           </p>
-          <p className="mt-5 text-2xl leading-7 dark:text-textLight-600">
+          <p className="mt-5 text-2xl leading-7 dark:text-textLight-600 max-md:text-center">
             <span
               className={`${boldClass} block after:not-italic after:content-['⚡']`}
             >
@@ -25,7 +26,7 @@ export function CuriositySection() {
             e Bruxaria de <span className={boldClass}>Hogwarts</span>, a{' '}
             <span className={boldClass}>Grifinória</span>.
           </p>
-          <p className="mt-5 text-sm leading-4 tracking-wide dark:text-textLight-600">
+          <p className="mt-5 text-sm leading-4 tracking-wide dark:text-textLight-600 max-md:text-center">
             Se não conhece nada sobre o universo mágico, confira o
             <a
               href="https://www.wizardingworld.com/"
@@ -37,7 +38,7 @@ export function CuriositySection() {
             </a>
           </p>
         </div>
-        <div className="h-[347px] w-[350px]">
+        <div className="h-[347px] w-[350px] max-md:mt-2 max-md:h-[225px] max-md:w-[203px]">
           <Model3D />
         </div>
       </section>
