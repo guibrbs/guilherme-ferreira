4e5ddfb86dd01dec602c60d1ac0ef80a068b38b1
guibrbs <200037994@aluno.unb.br>
2022-06-01 19:14:09 -0300

    :rocket: Setting up the banner layout in a component

---
+++ components/Banner/Layout.jsx
@@ -0,0 +1,32 @@
+import Link from "next/link"
+import { UnderlineLayout } from "../UnderlineLayout"
+
+const Layout = ({ children, homepage }) => (
+    <div className="flex h-full max-h-max w-full justify-center">
+      <section className={`mt-32 flex w-full max-w-customWidth flex-col items-center relative ${
+        homepage ? 'after:content-[""] after:w-full after:h-px after:bg-light-700 after:dark:bg-dark-100 after:absolute after:-bottom-16'
+        : ''
+      }`}>
+        <h1 className="text-3xl font-bold max-sm:text-2xl">
+          {homepage ? 'PRINCIPAIS PROJETOS' : 'PROJETOS'}
+        </h1>
+        {children}
+        {homepage 
+        ?
+        <>
+          <Link href={'/projetos'}>
+            <a className="mt-12 text-2xl font-medium relative group hover:brightness-110 duration-0 max-md:text-xl">
+              Explore mais projetos
+              <UnderlineLayout />  
+            </a>
+          </Link>
+        </>
+        :
+        <>
+        </>
+        }
+      </section>
+    </div>
+  )
+
+export default Layout
