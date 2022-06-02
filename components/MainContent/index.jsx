fcdcebe386243121cef2e5555cb84de637bbfcc6
guibrbs <200037994@aluno.unb.br>
2022-06-02 14:29:05 -0300

    :bug: Fixing a responsiviness bug

--- components/MainContent/index.jsx
+++ components/MainContent/index.jsx
@@ -10,19 +10,19 @@ export function MainContent(){
                 after:content[""] after:w-full after:h-px after:bg-light-700 dark:after:bg-dark-100 after:-bottom-16 max-sm:space-x-0 max-sm:px-10'>
                 <Avatar/>
                 <div className='w-full max-w-txtWidth max-sm:max-w-[350px]'>
-                    <p className='text-[2.9rem] leading-[2.9rem] font-bold max-sm:text-[32px]'>Guilherme Ferreira</p>
-                    <p className='text-2xl font-light dark:text-textLight-600 max-sm:text-[18px] max-sm:leading-3'>
+                    <p className='text-[2.9rem] leading-[2.9rem] font-bold max-sm:text-[30px] max-sm:leading-[30px]'>Guilherme Ferreira</p>
+                    <p className='text-2xl font-light dark:text-textLight-600 max-sm:text-[15px] max-sm:leading-[17px]'>
                         Web &amp; Mobile Developer (Fullstack)
                     </p>
                     <p className='w-full font-light text-2xl mt-6 dark:text-textLight-600 text-dark-500 leading-7 max-sm:text-xl max-sm:leading-6
-                    max-sm:text-justify'>
+                    max-sm:text-justify max-sm:mt-3'>
                         Sou estudante de <b className='dark:text-textLight-500 font-bold'>Engenharia de Software</b> pela 
                         <b className='dark:text-textLight-500 font-bold'> Universidade de Brasília</b>. 
                         Esse é o meu portfólio, onde você consegue encontrar alguns de meus projetos 
                         como <b className='dark:text-textLight-500 font-bold'>Desenvolvedor Web</b>. Se estiver procurando alguém 
                         <b className='dark:text-textLight-500 font-bold'> competente e focado</b>, hoje é seu dia de sorte, pois esse sou eu!
                     </p>
-                    <div className='flex mt-7 items-center space-x-3'>
+                    <div className='flex mt-7 items-center space-x-3 max-sm:mt-4'>
                         <button className='w-52 h-12 dark:bg-textLight-500 bg-dark-500 dark:text-dark-500 text-textLight-500 
                         font-bold text-xl rounded-lg hover:brightness-110 transition-all'>
                             Entre em contato
